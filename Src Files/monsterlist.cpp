87490ef8991af337eda67362c1a884f0950c20af
calvin224 <53382194+calvin224@users.noreply.github.com>
2021-02-05 21:23:53 +0000

    Update monsterlist.cpp

--- Src Files/monsterlist.cpp
+++ Src Files/monsterlist.cpp
@@ -3,7 +3,7 @@ using namespace std;
 MonsterList::MonsterList()
 {
     Enemy *Goblin(new Enemy("Goblin",4,2,100));
-    Enemy *skeletonSwordsman(new Enemy("Skeleton SwordsMan", 4,1,0));
+    Enemy *skeletonSwordsman(new Enemy("Skeleton SwordsMan", 4,1,95));
     level1mon.push_back(*Goblin);
     level1mon.push_back(*skeletonSwordsman);
 
